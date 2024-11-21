934098bca71160e35dbebb5987385d05b8e238db
Nguyen Van Duc <nguyenvanduc13475@gmail.com>
2024-11-21 20:26:43 +0700

    feat: implement place_item method for placing items into stock according to greedy algorithm

--- student_submissions/s2310790_2313873_2311011_2311770_2310271/policy2310790_2313873_2311011_2311770_2310271.py
+++ student_submissions/s2310790_2313873_2311011_2311770_2310271/policy2310790_2313873_2311011_2311770_2310271.py
@@ -4,12 +4,78 @@ import numpy
 
 def Policy2310790_2313873_2311011_2311770_2310271(Policy):
     def __init__(self):
-        # Student code here
-        pass
+        self.reset()
+
+    def reset(self):
+        self.first_time = True
+        self.step = 0
+        self.num_stocks = 0
+        self.num_products = 0
+        self.num_items = 0
+        self.stocks = []
+        self.stock_indices = None
+        self.products = []
+        self.product_indices = None
+        self.state_matrix = []
+        self.actions = []
 
     def get_action(self, observation, info):
-        # Student code here
         pass
 
-    # Student code here
-    # You can add more functions if needed
+    # Find the appropriate position in the stock to place the item,
+    # if no such position is found, return None.
+    def place_item(self, stock, product_width, product_height):
+        occupied_cells = stock["occupied_cells"]
+        num_row = len(occupied_cells)
+        num_col = len(occupied_cells[0])
+        verticals, horizontals = stock["grid"]
+
+        # Loop through each cell of the grid and find the cluster of cells that are not occupied,
+        # where the item can be placed.
+        for i in range(num_row):
+            for j in range(num_col):
+                if not occupied_cells[i][j]:
+                    # Check if there is enough space to the right of the current cell.
+                    right_edge = None
+                    for k in range(j + 1, num_col + 1):
+                        if occupied_cells[i][k - 1]:
+                            break
+
+                        if verticals[k] >= verticals[j] + product_width:
+                            right_edge = k
+                            break
+
+                    # Check if there is enough space above the current cell.
+                    if right_edge != None:
+                        for k in range(i + 1, num_row + 1):
+                            if occupied_cells[k - 1][j]:
+                                break
+
+                            if horizontals[k] >= horizontals[i] + product_height:
+                                # Update the grid after the item is placed.
+                                if verticals[j] + product_width < verticals[right_edge]:
+                                    verticals.add(verticals[j] + product_width)
+                                    for row in occupied_cells:
+                                        row.insert(right_edge, row[right_edge - 1])
+
+                                if horizontals[i] + product_height < horizontals[k]:
+                                    horizontals.add(horizontals[i] + product_height)
+                                    occupied_cells.insert(k, copy.deepcopy(occupied_cells[k - 1]))
+
+                                # Update the occupied cells after the item is placed.
+                                for m in range(i, k):
+                                    for n in range(j, right_edge):
+                                        occupied_cells[m][n] = True
+                                
+                                # Place the item
+                                stock["products"].append(
+                                    [verticals[j], horizontals[i], product_width, product_height]
+                                )
+                                # Update inner bound
+                                if stock["right_bound"] < verticals[j] + product_width:
+                                    stock["right_bound"] = verticals[j] + product_width
+                                if stock["top_bound"] < horizontals[i] + product_height:
+                                    stock["top_bound"] = horizontals[i] + product_height
+                                return True
+        
+        return False
